00c5c4424e87aa6fd834a5e61d1aaabbc0643d52
Яковлева Ксения <bezdenejnyhk@yandex.ru>
2022-09-13 18:01:46 +0300

    third commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,32 @@
-const OpenPopup = document.querySelector('.profile__edit');
-const ClosePopup = document.querySelector('.popup__close');
-const Popup = document.querySelector('.popup');
+const openpopup = document.querySelector('.profile__edit');
+const closepopup = document.querySelector('.popup__close');
+const popup = document.querySelector('.popup');
 
-let FormElement = document.querySelector('.profile__edit')
-let NameInput = document.querySelector('.popup__input_name');
-let JobInput = document.querySelector('.popup__input_description');
-let ButtonSave = document.querySelector('.popup__save');
-let Name = document.querySelector('.profile__info-name');
-let Job = document.querySelector('.profile__info-description');
+let formelement = document.querySelector('.popup__content')
+let nameinput = document.querySelector('.popup__input_name_pp');
+let jobinput = document.querySelector('.popup__input_description_pp');
+let buttonsave = document.querySelector('.popup__save');
+let profilename = document.querySelector('.profile__info-name');
+let profilejob = document.querySelector('.profile__info-description');
 
-let EditName = NameInput.value;
-let EditJob = JobInput.value;
+function openPopup() {
+    popup.classList.add('popup_opened')
+    nameinput.value = profilename.textContent;
+    jobinput.value = profilejob.textContent;
+}
 
-OpenPopup.addEventListener('click', function () {
-    Popup.classList.add('popup_opened');
-    EditName.textContent = NameInput.value;
-    EditJob.textContent = JobInput.value;
-})
+function closePopup() {
+    popup.classList.remove('popup_opened')
+}
 
-ClosePopup.addEventListener('click', function() {
-    Popup.classList.remove('popup_opened');
-})
+openpopup.addEventListener('click', openPopup);
+closepopup.addEventListener('click', closePopup);
 
-ButtonSave.addEventListener ('click', function formSubmitHandler (evt) {
-    evt.preventDefault(); 
-    Name.textContent = NameInput.value;
-    Job.textContent = JobInput.value;
-    Popup.classList.remove('popup_opened');
-})
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    profilename.textContent = nameinput.value;
+    profilejob.textContent = jobinput.value;
+    closePopup(popup);
+}
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-FormElement.addEventListener('submit', formSubmitHandler); 
+formelement.addEventListener('submit', formSubmitHandler);
