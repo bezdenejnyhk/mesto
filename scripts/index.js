e0be8133ee32f330ea53c199db939a26f9670b70
Яковлева Ксения <bezdenejnyhk@yandex.ru>
2022-09-13 21:27:20 +0300

    commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,32 @@
-const openpopup = document.querySelector('.profile__edit');
-const closepopup = document.querySelector('.popup__close');
+const openPopupButton = document.querySelector('.profile__edit');
+const closePopupButton = document.querySelector('.popup__close');
 const popup = document.querySelector('.popup');
 
-let formelement = document.querySelector('.popup__content')
-let nameinput = document.querySelector('.popup__input_name_pp');
-let jobinput = document.querySelector('.popup__input_description_pp');
-let buttonsave = document.querySelector('.popup__save');
-let profilename = document.querySelector('.profile__info-name');
-let profilejob = document.querySelector('.profile__info-description');
+let formElement = document.querySelector('.popup__content')
+let nameInput = document.querySelector('.popup__input_type_name');
+let jobInput = document.querySelector('.popup__input_type_description');
+let buttonSave = document.querySelector('.popup__save');
+let profileName = document.querySelector('.profile__info-name');
+let profileJob = document.querySelector('.profile__info-description');
 
 function openPopup() {
-    popup.classList.add('popup_opened')
-    nameinput.value = profilename.textContent;
-    jobinput.value = profilejob.textContent;
+    popup.classList.add('popup_opened');
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
 }
 
 function closePopup() {
-    popup.classList.remove('popup_opened')
+    popup.classList.remove('popup_opened');
 }
 
-openpopup.addEventListener('click', openPopup);
-closepopup.addEventListener('click', closePopup);
+openPopupButton.addEventListener('click', openPopup);
+closePopupButton.addEventListener('click', closePopup);
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
-    profilename.textContent = nameinput.value;
-    profilejob.textContent = jobinput.value;
+    profileName.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value;
     closePopup(popup);
 }
 
-formelement.addEventListener('submit', formSubmitHandler);
+formElement.addEventListener('submit', formSubmitHandler);
